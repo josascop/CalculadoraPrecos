2ebcac03732d03163cbb05850a6f9508dd01f512
josascop <jpalves_91@hotmail.com>
2023-01-05 22:30:42 -0300

    Add project files.

---
+++ CalculadoraPrecos/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace CalculadoraPrecos.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
